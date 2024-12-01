e0cef9a339333c1ebb0108da7ae7c3bb8cfa9fa7
MatCav01 <mattiacavicchio@gmail.com>
2024-12-01 21:00:44 +0100

    Adding train, validation and test functions

--- main.py
+++ main.py
@@ -2,8 +2,7 @@ from torchvision import transforms
 from torch.utils.data import DataLoader, random_split
 import torch
 from dataset_loading import CIM_Dataset
-from nn_loading import MultiLabelResnet18
-import matplotlib.pyplot as plt
+from nn_loading import MultiLabelResnet18, train_model, valid_model, test_model
 
 batch_size = 64
 learning_rate = 0.001
@@ -16,6 +15,7 @@ data_transform = transforms.Compose([
 ])
 
 dataset = CIM_Dataset(root='./DATABASE_CIM', valid=False, transform=data_transform)
+n_classes = len(dataset.classes)
 with torch.Generator() as generator:
     generator.manual_seed(42)
     train_dataset, test_dataset = random_split(dataset, lengths=[0.8, 0.2], generator=generator)
@@ -25,10 +25,14 @@ train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
 valid_loader = DataLoader(valid_dataset, batch_size=batch_size, shuffle=False)
 test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
 
-model_net = MultiLabelResnet18(n_classes=len(dataset.classes)).to(device)
+model_net = MultiLabelResnet18(n_classes).to(device)
 criterion = torch.nn.BCELoss()
 optimizer = torch.optim.Adam(params=model_net.parameters(), lr=learning_rate)
 
 # train and valid
 for epoch in range(n_epochs):
-    model_net.train()
+    train_loss, train_acc = train_model(model_net, train_loader, criterion, optimizer, device, n_classes)
+    valid_loss, valid_acc = valid_model(model_net, valid_loader, criterion, device, n_classes)
+    print(f'Epoch {epoch + 1}/{n_epochs}:')
+    print(f'\tTrain Loss: {train_loss:.6f}\n\tTrain Accuracy: {train_acc}')
+    print(f'\tValid Loss: {valid_loss:.6f}\n\tValid Accuracy: {valid_acc}\n')
--- nn_loading.py
+++ nn_loading.py
@@ -1,15 +1,86 @@
 from torchvision import models
-import torch.nn as nn
+import torch
+from torchmetrics.classification.accuracy import MultilabelAccuracy
 
-class MultiLabelResnet18(nn.Module):
+class MultiLabelResnet18(torch.nn.Module):
     def __init__(self, n_classes):
         super().__init__()
         self.resnet = models.resnet18(weights=models.ResNet18_Weights.IMAGENET1K_V1)
-        self.resnet.fc = nn.Sequential(
-            nn.Linear(self.resnet.fc.in_features, n_classes),
-            nn.Sigmoid()
+        self.resnet.fc = torch.nn.Sequential(
+            torch.nn.Linear(self.resnet.fc.in_features, n_classes),
+            torch.nn.Sigmoid()
         )
     
     def forward(self, x):
         out = self.resnet(x)
         return out
+
+def train_model(model, train_loader, criterion, optimizer, device, n_classes):
+    model.train()
+    train_loss = 0.0
+    mla = MultilabelAccuracy(num_labels=n_classes, average=None)
+    accuracy = [0.0 for _ in range(n_classes)]
+    accuracy = torch.tensor(accuracy, device=device)
+    
+    for images, labels in train_loader:
+        images = images.to(device)
+        labels = labels.to(device)
+
+        optimizer.zero_grad()
+        outputs = model(images)
+        loss = criterion(outputs, labels)
+
+        loss.backward()
+        optimizer.step()
+
+        train_loss += loss.item()
+        accuracy += mla(outputs, labels)
+
+    train_loss /= len(train_loader)
+    accuracy /= len(train_loader)
+
+    return train_loss, accuracy.numpy()
+
+def valid_model(model, valid_loader, criterion, device, n_classes):
+    model.eval()
+    valid_loss = 0.0
+    mla = MultilabelAccuracy(num_labels=n_classes, average=None)
+    accuracy = [0.0 for _ in range(n_classes)]
+    accuracy = torch.tensor(accuracy, device=device)
+    
+    for images, labels in valid_loader:
+        images = images.to(device)
+        labels = labels.to(device)
+
+        outputs = model(images)
+        loss = criterion(outputs, labels)
+
+        valid_loss += loss.item()
+        accuracy += mla(outputs, labels)
+
+    valid_loss /= len(valid_loader)
+    accuracy /= len(valid_loader)
+
+    return valid_loss, accuracy.numpy()
+
+def test_model(model, test_loader, criterion, device, n_classes):
+    model.eval()
+    test_loss = 0.0
+    mla = MultilabelAccuracy(num_labels=n_classes, average=None)
+    accuracy = [0.0 for _ in range(n_classes)]
+    accuracy = torch.tensor(accuracy, device=device)
+    
+    for images, labels in test_loader:
+        images = images.to(device)
+        labels = labels.to(device)
+
+        outputs = model(images)
+        loss = criterion(outputs, labels)
+
+        test_loss += loss.item()
+        accuracy += mla(outputs, labels)
+
+    test_loss /= len(test_loader)
+    accuracy /= len(test_loader)
+
+    return test_loss, accuracy.numpy()
